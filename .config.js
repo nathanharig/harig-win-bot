cd9a0f9e0859ac143ae8990d1f1dd16f9c6e9efb
Nathan Harig <nathanharig@gmail.com>
2017-11-08 15:10:31 -0500

    Third

---
+++ .config.js
@@ -0,0 +1,13 @@
+-//config.js
+ -/** TWITTER APP CONFIGURATION
+ - * consumer_key
+ - * consumer_secret
+ - * access_token
+ - * access_token_secret
+ - */
+ -module.exports = {
+ -  consumer_key: 'mYaL9agoXP2Sd2Ve1AuirXZko',
+ -  consumer_secret: 'AuwSFoAkwk6I47txf7EjM1TmX7ZWToFf9fwKuKZO4qYx86AbUB',
+ -  access_token: '927918236456366080-cMmqUnPjRAfJM73EczmwRi9og3DCUsP',
+ -  access_token_secret: 'lYXxqInoqW7G1vL5cnMW1GRyCN5jdtZcuhYdNvUhiJLtz'
+ -}

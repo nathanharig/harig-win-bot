32108e2b07c55b774f0b2d25071e4c16cc724c4c
Nathan Harig <nathanharig@gmail.com>
2017-11-11 09:32:45 -0500

    Updated bot for time, search parameters, and added unfollow/delete function

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ This is a bot that I made in node.js to play around with searching twitter for c
 
 If you are interested in using this bot
 
-1) Clone the repository to your local computer (you will need at least node.js 8 installed to use this code. 
-2) Run npm install in the local directory. 
+1) Clone the repository to your local computer (you will need at least node.js 8 installed to use this code.
+2) Run npm install in the local directory.
 3) Create an app on apps.twitter.com
 4) Create a new local file in the base bot directory, titled .config.js, which contains the following (consumer keys & access tokens come from apps.twitter.com):
 	 ```
@@ -13,15 +13,16 @@ If you are interested in using this bot
 	   consumer_secret: 'YOUR-TWITTER-SECRET-HERE',
 	   access_token: 'YOUR-TWITTER-ACCESS-TOKEN-HERE',
 	   access_token_secret: 'YOUR-TWITTER-TOKEN-SECRET-HERE'
-	} 
+	}
 	```
 5) Launch the node command line, navigate to the directory, and type in npm start. Voila, you should be working!
 
-Tips: 
+Tips:
 
-1) Terms can be edited in the mySearch function of bot.js. Edit any item in the array rtTerms. Please note, the bot by default only fetches every 25 minutes, and only the first result (most recent) from each field is tweeted. To change the interval, edit the setInterval at the bottom bot.js, time is in miliseocnds
-2) The bot has a built in check to make sure it's not tweeting old links. This check is in the bot.js recentDatesOnly function and returns a value, currently checking to make sure that a status isn't more than 4 days old. 
-3) The bot also is geo limited to check around a 500 mile radius from Carlisle, PA- my hometown. You can edit this in the retweet function by modifying geocode params as a string that follows the format 'lat,lon,radius' but be sure to include either mi or km at the end of radius so that your function actually works. 
-4) Bot.js includes a "blockList" function that hides users you don't want to retweet. By default, this includes my test account, my personal account, and an annoying giveaway site. Edit the array by adding whomever you'd like. These tweets will not be retweeted or followed. 
+1) Terms can be edited in the mySearch function of bot.js. Edit any item in the array rtTerms and send you geo parameters to the function retweet.  Only the first result (most recent) from each field is tweeted. To change the interval, edit the setInterval at the bottom bot.js, time is in miliseocnds.
+2) The bot has a built in check to make sure it's not tweeting old links. This check is in the bot.js recentDatesOnly function and returns a value, currently checking to make sure that a status isn't more than 4 days old.
+3) The bot also is geo limited to check around a 500 mile radius from Carlisle, PA- my hometown. You can edit this in the retweet function by modifying geocode params as a string that follows the format 'lat,lon,radius' but be sure to include either mi or km at the end of radius so that your function actually works.
+4) Bot.js includes a "blockList" function that hides users you don't want to retweet. By default, this includes my test account, my personal account, and an annoying giveaway site. Edit the array by adding whomever you'd like. These tweets will not be retweeted or followed.
+5) The Bot now includes an unfollow/delete status function that ones initially and again eveyr week. This helps keep follows trim and your tweets from being too obnoxious.
 
 Thanks for checking this out!
--- bot.js
+++ bot.js
@@ -1,19 +1,21 @@
 var twit = require('twit');
 var config = require ('./.config.js');
 var Twitter = new twit(config);
+var lastOne = new String();
 
 
 var mySearch = function(){
   rtTerms = ['win tickets rt', 'win tix rt' , 'win pair rt', '#WinItWednesday' , '#FreebieFriday']
   var currentDate = new Date(Date.now());
   var formatDate = currentDate.toUTCString();
-  console.log(`/// Current Date is ${formatDate} ///`);
+  console.log(`####### MySearch /// Current Date is ${formatDate} /// #######`);
   for (var i in rtTerms)
   {
     var params =rtTerms[i];
     //  retweet(params)
-    retweet(params)
+    retweet(params,'40.2,-77.2,200mi')
   }
+  //console.log('mySearch Complete');
 }
 
 var recentDatesOnly = function(statusDate){
@@ -24,30 +26,32 @@ var recentDatesOnly = function(statusDate){
   {
     isCurrent = true;
   }
+  // console.log('recentDatesOnly Complete');
   return isCurrent;
 }
 
 var blockList = function(screenName){
-  var blocked = ['NathanWinsStuff', 'nathanharig', 'your_home'];
+  var blocked = ['NathanWinsStuff', 'nathanharig', 'your_home', 'TempleEDM', 'HoneyBunnyTV'];
   var check = true;
   for (i in blocked) {
     if (blocked[i] === screenName) {
       check = false;
     }
   }
+//  console.log('blockList Complete');
   return check;
 }
 
-var retweet = function(params) {
+var retweet = function(params, geo) {
   var params = {
     q: params,
     result_type: 'recent',
     lang: 'en',
-    geocode:'40.2,-77.2,500mi',
-    }
+    geocode: geo,
+  }
 
   Twitter.get('search/tweets', params, function(err, data) {
-
+    try {
       if (!err && data.statuses[0].retweeted_status != null) {
         var dated = recentDatesOnly(data.statuses[0].retweeted_status.created_at)
         var retweetId = data.statuses[0].retweeted_status.id_str;
@@ -72,7 +76,27 @@ var retweet = function(params) {
       }
       else {
       }
+    }
+    catch(e) {
+      console.log(`${data.statuses[0]} and \n ${e}`);
+    }
   });
+
+//console.log('retweet Complete');
+}
+
+var mySearch2 = function(){
+  rtTerms = ['win xbox rt', '"win an iphone"' , 'win airfare rt', '#Giveaway' , '#Sweepstakes']
+  var currentDate = new Date(Date.now());
+  var formatDate = currentDate.toUTCString();
+  console.log(`####### MySearch 2 /// Current Date is ${formatDate} /// #######`);
+  for (var i in rtTerms)
+  {
+    var params =rtTerms[i];
+    //  retweet(params)
+    retweet(params,'39.8283,-98.5795,2200km')
+  }
+//  console.log('mySearch2 Complete');
 }
 
 var followUser = function (screenName, retweeted, params)
@@ -89,7 +113,154 @@ var followUser = function (screenName, retweeted, params)
     }
     console.log(`\n ||||| Param- ${params.q} \n ||||  ${resp.following} status following ${resp.screen_name} \n ||| for  ${someText} \n || originally sent ${retweeted.created_at} \n  `);
   });
+//  console.log('followUser Complete');
 }
 
-mySearch();
-setInterval(mySearch, 1500000);
+var oneWeekTweets = function(statusDate){
+  var currentDate = new Date(Date.now() - 604800000);
+  var checkDate = new Date(statusDate);
+  var isCurrent = false;
+  if (checkDate >= currentDate)
+  {
+    isCurrent = true;
+  }
+//  console.log('oneWeekTweets Complete');
+  return isCurrent;
+}
+
+var unFollowCheck = function (sinceID) {
+  var followedTweets = [];
+  if (!sinceID) {
+
+    Twitter.get('statuses/user_timeline', {
+      screen_name: 'NathanWinsStuff',
+      count: 200 } , function (err, resp) {
+        if (!err) {
+
+          for (i in resp)
+          {
+            followedTweets.push(resp[i]);
+          }
+          console.log(`@@@@ First Run - Checking start time- ${followedTweets[0].created_at} @@@@`);
+          for (var i = 0; i<followedTweets.length - 1; i++)
+          {
+            var shorten = followedTweets[i].retweeted_status;
+            var tweetID = followedTweets[i].id_str;
+            try {
+              var check = oneWeekTweets(shorten.created_at);
+            }
+            catch(e) {
+              var check = false;
+            }
+          //  console.log(`Tweet created ${followedTweets[i].created_at} with status of ${check} at index ${i}`);
+            doUnfollow(shorten, check);
+            doDelete(tweetID, check);
+            lastOne = followedTweets[followedTweets.length - 1];
+          }
+
+
+        }
+        else {
+          console.log(err);
+        }
+      //  console.log(`Complete with ${lastOne.id_str} and ${followedTweets.length}`);
+        if (followedTweets.length > 1) {
+          unFollowCheck(lastOne.id_str);
+        }
+
+      });
+
+
+  }
+  else {
+    followedTweets = [];
+    Twitter.get('statuses/user_timeline', {
+      screen_name: 'NathanWinsStuff',
+      count: 200,
+      max_id: sinceID } , function (err, resp) {
+        if (!err) {
+
+          for (i in resp)
+          {
+            followedTweets.push(resp[i]);
+          }
+          console.log(`@@@@ Checking start time- ${followedTweets[0].created_at} @@@@`);
+          for (var i = 0; i<followedTweets.length - 1; i++)
+          {
+            var shorten = followedTweets[i].retweeted_status;
+            var tweetID = followedTweets[i].id_str;
+            try {
+              var check = oneWeekTweets(followedTweets[i].created_at);
+            }
+            catch(e) {
+              var check = false;
+            }
+          //  console.log(`Tweet created ${followedTweets[i].created_at} with status of ${check} at index ${i}`);
+            doUnfollow(shorten, check);
+            doDelete(tweetID, check);
+            lastOne = followedTweets[followedTweets.length - 1];
+          }
+
+
+        }
+        else {
+          console.log(err);
+        }
+        // console.log(`Complete with ${lastOne.id_str} and ${followedTweets.length}`);
+        if (followedTweets.length > 1) {
+          unFollowCheck(lastOne.id_str);
+        }
+
+      });
+
+    }
+//    console.log('unFollowCheck Complete');
+  }
+
+  var doUnfollow = function (shorten, check) {
+    if (check != true) {
+      Twitter.post('friendships/destroy', {
+        screen_name: shorten.user.screen_name
+      } , function (err, resp) {
+        if (!err) {
+          console.log(`Successfully unfollowed ${shorten.user.screen_name}`);
+        }
+        else {
+          console.log(err);
+        }
+      });
+    }
+//    console.log('doUnfollow Complete');
+  }
+
+  var doDelete = function (tweetID, check){
+    if (check!= true)
+    {
+      Twitter.post('statuses/destroy', {
+        id: tweetID }, function (err, resp) {
+          if (!err)
+          {
+            console.log(`Tweet Deleted (id) ${tweetID}`);
+          }
+          else {
+            console.log(err);
+          }
+        });
+      }
+    //  console.log('doDelete Complete');
+    }
+
+    var getKeys = function(obj){
+      var keys = [];
+      for(var key in obj){
+        keys.push(key);
+      }
+      console.log(keys);
+    }
+
+    mySearch();
+    mySearch2();
+    unFollowCheck();
+    setInterval(unFollowCheck, 86400000);
+    setInterval(mySearch, 150000);
+    setInterval(mySearch2, 150000);
